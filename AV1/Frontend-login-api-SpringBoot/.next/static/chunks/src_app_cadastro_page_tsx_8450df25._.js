32d26daece50fa6b5afc1c3dd36e81cb10644a6b
titolc <christopherlindoso@gmail.com>
2025-05-05 22:07:09 -0300

    Add files via upload

---
+++ AV1/Frontend-login-api-SpringBoot/.next/static/chunks/src_app_cadastro_page_tsx_8450df25._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_dd8b6d64._.js"
+],
+    source: "dynamic"
+});
